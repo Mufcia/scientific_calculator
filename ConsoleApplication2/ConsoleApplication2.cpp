889a03a13cdbe2ddbb3ab306e0077c5dee1b74b1
Aleksandra Zięba <01140673@pw.edu.pl>
2021-08-15 18:12:39 +0200

    precision

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -40,7 +40,7 @@ int main()
 	int  a1, b1, wybor, wynik = 1;
 	float a, b;
 	string pomocniczaa, pomocniczab;
-	int podziel;
+	int podziel = 1;
 
 	cout << "1. Dodawanie " << endl << "2. Odejmowanie " << endl;
 	cin >> wybor;
@@ -51,7 +51,7 @@ int main()
 	cout << endl;
 
 	int szczfany1, szczfany2;
-	int poka1, poka2, tutej1 = 0, tutej2 = 0, doasm1, doasm2, tylemnoz1, tylemnoz2;
+	int poka1, poka2, tutej1 = 0, tutej2 = 0, doasm1, doasm2, tylemnoz1 = 0, tylemnoz2 = 0;
 
 	//floaty do stringstream
 
@@ -67,45 +67,56 @@ int main()
 	for (int i = 0; i < poka1; i++) {
 		if (pomocniczaa[i] == ',' || pomocniczaa[i] == '.') {
 			tutej1 = i;
+			tylemnoz1 = poka1 - tutej1 - 1;
 		}
 	}
-	tylemnoz1 = poka1 - tutej1 - 1;
 	cout << tylemnoz1 << endl;
 
 	for (int i = 0; i < poka2; i++) {
 		if (pomocniczab[i] == ',' || pomocniczab[i] == '.') {
 			tutej2 = i;
+			tylemnoz2 = poka2 - tutej2 - 1;
 		}
 	}
-	tylemnoz2 = poka2 - tutej2 - 1;
 	cout << tylemnoz2 << endl;
 
 	pomocniczaa.erase(remove(pomocniczaa.begin(), pomocniczaa.end(), '.'), pomocniczaa.end());
 	pomocniczab.erase(remove(pomocniczab.begin(), pomocniczab.end(), '.'), pomocniczab.end());
-	pomocniczaa.erase(remove(pomocniczaa.begin(), pomocniczaa.end(), ','), pomocniczaa.end());
-	pomocniczab.erase(remove(pomocniczab.begin(), pomocniczab.end(), ','), pomocniczab.end());
 
 	szczfany1 = stoi(pomocniczaa);
 	szczfany2 = stoi(pomocniczab);
 	cout << szczfany1 << endl;
 	cout << szczfany2 << endl;
 
+	
+
 	//wyrównywanei długości części ułamkowej
-	if (tylemnoz1 >= tylemnoz2) {
+	if (tylemnoz1 > tylemnoz2) {
+		if ((tylemnoz1 + poka2 - tylemnoz2) > 9) {
+			szczfany1 = stoi(pomocniczaa.substr(0, 10 - poka2));
+			tylemnoz1 = 9 - poka2; 
+		}
 		podziel = tylemnoz1;
 		for (int j = 0; j < tylemnoz1 - tylemnoz2; j++) {
 			//szczfany1 *= 10;
 			szczfany2 *= 10;
 		}
 	}
-	else
+	else if (tylemnoz1 < tylemnoz2)
 	{
+		if ((tylemnoz2 + poka1 - tylemnoz1) > 9) {
+			szczfany2 = stoi(pomocniczab.substr(0, 10 - poka1));
+			tylemnoz2 = 9 - poka1;
+		}
 		podziel = tylemnoz2;
 		for (int j = 0; j < tylemnoz2 - tylemnoz1; j++) {
 			szczfany1 *= 10;
 			//szczfany2 *= 10;
 		}
 	}
+	else podziel = tylemnoz1;
+
+	cout << szczfany1 << " " << szczfany2 << endl;
 
 	switch (wybor) {
 	case 1:
@@ -115,12 +126,12 @@ int main()
 		wynik = odejmowanie(szczfany1, szczfany2);
 		break;
 	}
+	cout << wynik << endl;
 	string swynik = to_string(wynik);
-	int poka;
-	if (poka1 > poka2)
-		poka = poka1;
-	else poka = poka2; 
-	swynik.insert(poka-podziel-1, ".");
+	if (swynik.length() <= podziel && wynik<0)
+		for (int i =0; i <podziel-1; i++)
+			swynik.insert(1, "0");
+	swynik.insert(swynik.length()-podziel, ".");
 
 	cout << swynik << endl; 
 	return 0;
